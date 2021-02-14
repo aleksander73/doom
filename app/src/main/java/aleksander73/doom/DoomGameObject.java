f9aa1e068e9bf9439fc8f521326722385fc89c68
aleksander73 <olekdobkowski@gmail.com>
2021-02-14 13:20:51 +0100

    Add doom game object

---
+++ app/src/main/java/aleksander73/doom/DoomGameObject.java
@@ -0,0 +1,26 @@
+package aleksander73.doom;
+
+import aleksander73.vector.core.GameObject;
+import aleksander73.vector.core.Transform;
+import aleksander73.vector.rendering.materials.Colour;
+import aleksander73.vector.rendering.materials.Material;
+import aleksander73.vector.rendering.materials.Texture;
+import aleksander73.vector.rendering.mesh.Mesh;
+import aleksander73.vector.rendering.renderers.MeshRenderer;
+import aleksander73.vector.rendering.shaders.Shader;
+
+public class DoomGameObject extends GameObject {
+    private final Texture mainTexture;
+
+    public DoomGameObject(String name, Transform transform, Mesh mesh, Shader shader, Colour colour, Texture mainTexture) {
+        super(name);
+        Material material = new Material(colour, mainTexture, shader);
+        MeshRenderer meshRenderer = new MeshRenderer(mesh);
+        this.addComponents(transform, mesh, material, meshRenderer);
+        this.mainTexture = material.getTexture();
+    }
+
+    public Texture getMainTexture() {
+        return mainTexture;
+    }
+}
